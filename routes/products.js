1388320c0b9723c128745e170c6b45d4988d5bcd
samuel ho <samuelho343@gmail.com>
2023-02-18 22:29:42 -0800

    products api fix

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,7 @@ const controllers = require('../controller');
 
 const router = express.Router();
 
-const url = 'http://localhost:3020';
+const url = 'http://localhost:3020/products';
 //Products
 
 router.get('*', (req, res) => {
